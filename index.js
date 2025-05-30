432ae72138bbb2fbbca45431d676457dfbe0e531
yash <yashmevat16@gmail.com>
2025-05-30 15:52:10 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
